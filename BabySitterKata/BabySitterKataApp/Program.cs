f94b42aca5b422db799d897ad2a737f071b55097
kRenelt <43582922+kRenelt@users.noreply.github.com>
2018-10-24 11:31:33 -0500

    Finished the Babysitter kata

--- BabySitterKata/BabySitterKataApp/Program.cs
+++ BabySitterKata/BabySitterKataApp/Program.cs
@@ -11,21 +11,52 @@ namespace BabySitterKataApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter Start Time");
-            string startTimeString = Console.ReadLine();
-            int startTime = Int32.Parse(startTimeString);
-            Console.WriteLine("Please enter an end time");
-            string endTimeString = Console.ReadLine();
-            int endTime = Int32.Parse(endTimeString);
-            FamilyA familyA = new FamilyA();
-            FamilyB familyB = new FamilyB();
-            FamilyC familyC = new FamilyC();
+            int totalEarned = 0;
+            bool keepPlaying = true;
+            do
+            {
+                Console.WriteLine("Please Enter Start Time.. eg 5, 6, 7");
+                string startTimeString = Console.ReadLine();
 
-            // int totalEarned = familyA.moneyEarned(startTime, endTime);
-            int totalEarned = familyC.moneyEarned(startTime, endTime);
-            Console.WriteLine("you earned = " + totalEarned);
+                int startTime = Int32.Parse(startTimeString);
+                if (startTime < 5)
+                {
+                    Console.WriteLine("We are sorry our babysitters cannot start until after 5:00 PM");
+                    continue;
+                }
+                Console.WriteLine("Please enter an end time");
+                string endTimeString = Console.ReadLine();
+                int endTime = Int32.Parse(endTimeString);
 
-            Console.ReadLine();
+                if (endTime > 4)
+                {
+                    Console.WriteLine("We are sorry our babysitters cannot Work after 4:00 AM");
+                    continue;
+                }
+
+                Console.WriteLine("Please enter your Family type, A, B or C");
+                string tempFamilyType = Console.ReadLine();
+                string familyType = tempFamilyType.ToUpper();
+      
+                FamilyA familyA = new FamilyA();
+                FamilyB familyB = new FamilyB();
+                FamilyC familyC = new FamilyC();
+                if (familyType == "A")
+                    totalEarned = familyA.moneyEarned(startTime, endTime);
+                else if (familyType == "B")
+                    totalEarned = familyB.moneyEarned(startTime, endTime);
+                else if (familyType == "C")
+                    totalEarned = familyC.moneyEarned(startTime, endTime);
+
+                Console.WriteLine("Total amount earned = " + totalEarned);
+                Console.WriteLine("would you like to schedule another Apt'");
+                string playAgain = Console.ReadLine();
+                playAgain = playAgain.ToUpper();
+               
+                if (playAgain == "N")
+                    keepPlaying = false;
+            }
+            while (keepPlaying);
         }
     }
 }
